401cd8adfbca9384162947edf77f9f032e4eaa29
narela <narela315@gmail.com>
2021-08-25 19:09:52 -0300

    metedo para saber si un string es un palindromo

--- Helper.js
+++ Helper.js
@@ -30,23 +30,65 @@
         return newArray;
     }
     desanidarArray(array){ //Dado un arreglo multidimensional, obtener todos los valores en un nuevo arreglo de una sola dimension. Usar recursividad
-        var newArray = [];
         for(var i = 0; i < array.length; i++){
             if(!Array.isArray(array[i])){
                 this.initArray.push(array[i])
             }
             else this.desanidarArray(array[i])
         }
+        return this.initArray 
+    }
+    truncateStr(str){ // saca punto y comas de un string, y convierte todo a minuscula
+        var minuscula = str.toLowerCase()
+        var string = minuscula.replace(/,/g, "");
+        var newString = string.replace(/\./g, '');
+        return newString;
+    }
+    masRepetido(string){ //Dado un string, devolver un objeto/array que indique la palabra que mas veces se repite, y su cantidad.
+        var newString = this.truncateStr(string)
+        var array = newString.split(" ");
+        var obj = {}
+        array.forEach(function(s){  // pone en el objeto la key: es cada palabra, y valor: es el numero de repeticion
+            obj[s] = (obj[s] || 0) + 1;
+          });
         
-        return this.initArray
+        var masRepeticiones = Math.max(...(Object.values(obj)))  // obtiene el valor mas alto
+        var stringRepetido = Object.keys(obj).find(key => obj[key] === masRepeticiones)  //obtiene la key del valor mas alto
+        var newObj = {}
+        newObj[stringRepetido] = masRepeticiones; // armo el nuevo objeto con el string y el numero de repeticiones que tiene que retornar
+        return newObj
+    }
+    palindromo(string){
+        
+        var str = this.truncateStr(string);
+        var newStr = str.replace(/ /g, "")
+        if(string.length === 1){
+            console.log('Si es un palindromo')
+            return 'Si es un palindromo'
+        }
+        else if(newStr[0] === newStr[newStr.length-1]){
+            var primer = newStr.slice(1)
+            var ultimo = primer.slice(0, -1)
+            return this.palindromo(ultimo)
+            
+        }
+        else
+        console.log('No es un palindromo')
+        return 'No es un palidromo'
+        
+        
         
     }
 }
 
 
 
-var newHelper = new Helper()
-console.log(newHelper.multiplicate(4, 5))
-console.log(newHelper.mayorNum([16, 3, 5, 8, 15]))
-console.log(newHelper.dejarString([undefined, null, 5, 'perro', 6, 'gato', null, 'conejo', false, 0 , 3, 4, 'undefined']))
-console.log(newHelper.desanidarArray([1, [2, [3, 4]], 'hola', [1, 'buenos dias']]))
+var newHelper = new Helper();
+
+//console.log(newHelper.multiplicate(6, 5))
+//console.log(newHelper.mayorNum([16, 3, 19, 8, 15]))
+//console.log(newHelper.dejarString([undefined, null, 5, 'perro', 6, 'gato', null, 'conejo', false, 0 , 3, 4, 'undefined']))
+//console.log(newHelper.desanidarArray([1, [2, [3, 4]], 'hola', [1, 'buenos dias']]))
+//console.log(newHelper.masRepetido('Este es un string, el cual es un string, donde se repite muchas veces la palabra string es.'))
+//newHelper.truncateStr('Este es un string, el cual es un string, donde se repite muchas veces la palabra es')
+newHelper.palindromo('Somos, o no somos')
