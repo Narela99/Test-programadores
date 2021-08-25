630751c3288a06642dc714f2071218dc0012ad0e
narela <narela315@gmail.com>
2021-08-24 23:42:22 -0300

    eliminar todos los "undefined", "null", "false" y 0 (no sus valores en string). Iterar solo 1 vez

--- Helper.js
+++ Helper.js
@@ -16,8 +16,19 @@
         return mayor;
 
     }
+    dejarString(array){
+        var newArray = [];
+        for(var i = 0; i < array.length; i++){
+            if(typeof(array[i]) === 'string' || typeof(array[i]) === 'number' && array[i] != 0){
+                newArray.push(array[i])
+            }
+        }
+        console.log(newArray)
+        return newArray;
+    }
 }
 
 var newHelper = new Helper()
 newHelper.multiplicate(4, 5)
 newHelper.mayorNum([16, 3, 5, 8, 15])
+newHelper.dejarString([undefined, null, 5, 'perro', 6, 'gato', null, 'conejo', false, 0 , 3, 4, 'undefined'])
